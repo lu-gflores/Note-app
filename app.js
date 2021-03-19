c006638cd8b05d776ea06cf984bd02b8d0663b5d
George Flores <georgeflores886@gmail.com>
2021-03-19 11:38:43 -0400

    installed yargs

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+const chalk = require('chalk')
 const notes = require('./notes')
-const msg = notes()
-console.log(msg)
+
+const command = process.argv[2]
+
+//argv is an array with all of the arguments provided by the user
+console.log(process.argv)
+
+if (command === 'add') {
+    console.log('Adding Note')
+} else if (command === 'remove') {
+    console.log('Removing Note')
+}
