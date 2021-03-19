a6311e0399f86ba36c124cc045eb27f48f7814b0
George Flores <georgeflores886@gmail.com>
2021-03-19 12:13:26 -0400

    setup yarg commands for notes

--- app.js
+++ app.js
@@ -1,13 +1,63 @@
 const chalk = require('chalk')
+const { command, describe } = require('yargs')
+const yargs = require('yargs')
 const notes = require('./notes')
 
-const command = process.argv[2]
+//customize yargs version
+yargs.version('1.1.0')
+
+//create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Write in the note\'s body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
+    }
+})
+
+//create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing note')
+    }
+})
+
+//list command
+yargs.command({
+    command: 'list',
+    describe: 'List all Notes avaliable',
+    handler: function () {
+        console.log('Listing out all notes')
+    }
+})
+
+//read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading note...')
+    }
+})
 
 //argv is an array with all of the arguments provided by the user
-console.log(process.argv)
+//console.log(process.argv)
+yargs.parse()
+//console.log(yargs.argv)
 
-if (command === 'add') {
-    console.log('Adding Note')
-} else if (command === 'remove') {
-    console.log('Removing Note')
-}
+
